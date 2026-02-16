bb367aacbae969070e54ad13bd8532a5328ac191
Kyle Collins <kgc78423@gmail.com>
2026-02-16 18:54:15 -0500

    Added 8 tables

--- Data/RDB/CreateTables_Collins.sql
+++ Data/RDB/CreateTables_Collins.sql
@@ -1,44 +1,105 @@
-use Mist460_RDB_Collins;
+use Mist460_RDB_Collins; 
+
+-- Order matters (Why?)
+IF OBJECT_ID('Section') IS NOT NULL DROP TABLE Section;
+IF OBJECT_ID('Instructor') IS NOT NULL DROP TABLE Instructor;
+IF OBJECT_ID('Course') IS NOT NULL DROP TABLE Course;
+IF OBJECT_ID('Major') IS NOT NULL DROP TABLE Major;
+IF OBJECT_ID('Alum') IS NOT NULL DROP TABLE Alum;
+IF OBJECT_ID('Advisor') IS NOT NULL DROP TABLE Advisor;
+IF OBJECT_ID('Student') IS NOT NULL DROP TABLE Student;
+IF OBJECT_ID('AppUser') IS NOT NULL DROP TABLE AppUser;
+
 GO
 
--- Drop tables if they exist
-if object_id('Student') is not null
-    drop table Student;
-if object_id('Appuser') is not null
-    drop table Appuser;
+CREATE TABLE AppUser (
+    AppUserID       INT IDENTITY(1,1) 
+        CONSTRAINT PK_AppUser PRIMARY KEY,
+    Firstname        NVARCHAR(50)  NOT NULL,
+    Lastname        NVARCHAR(50)  NOT NULL,
+    Email           NVARCHAR(100)  NOT NULL 
+        CONSTRAINT UK_AppUser_Email UNIQUE,
+    PhoneNumber     NVARCHAR(20)   NULL,
+    PasswordHash    VARBINARY(256)  NOT NULL,
+    UserRole        NVARCHAR(20)   NOT NULL
+        CONSTRAINT CK_AppUser_UserRole CHECK (UserRole IN (N'Student', N'Advisor',N'Alum'))
+);
 GO
 
-create table Appuser
-(
-    AppuserID int identity(1,1),
+CREATE TABLE Student (
+    StudentID               INT 
+        CONSTRAINT PK_Student PRIMARY KEY
+        CONSTRAINT FK_Student_AppUser FOREIGN KEY 
+        REFERENCES AppUser(AppUserID),
+    TotalCreditsCompleted   INT NOT NULL
+        CONSTRAINT DF_Student_Credits DEFAULT (0)
+        CONSTRAINT CK_Student_TCC CHECK (TotalCreditsCompleted >= 0),
+    GraduationSemesterYear NVARCHAR(25) not null,
+    OverallGPA decimal(3,2) not null 
+        constraint DF_Student_OverallGPA DEFAULT 0.00,
+    MajorGPA decimal(3,2) not null 
+        constraint DF_Student_MajorGPA DEFAULT 0.00
+);
+GO
+
+CREATE TABLE Advisor (
+    AdvisorID   INT CONSTRAINT PK_Advisor PRIMARY KEY,
+    CONSTRAINT FK_Advisor_AppUser FOREIGN KEY (AdvisorID)
+        REFERENCES AppUser(AppUserID) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE Alum (
+    AlumID              INT CONSTRAINT PK_Alum PRIMARY KEY,
+    GraduationSemesterYear      NVARCHAR(25) NOT NULL,
+    CONSTRAINT FK_Alum_AppUser FOREIGN KEY (AlumID)
+        REFERENCES AppUser(AppUserID) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE Major (
+    MajorID     INT IDENTITY(1,1) CONSTRAINT PK_Major PRIMARY KEY,
+    MajorName   NVARCHAR(200) NOT NULL CONSTRAINT UK_Major_Name UNIQUE
+);
+GO
+
+CREATE TABLE Course (
+    CourseID        INT IDENTITY(1,1) CONSTRAINT PK_Course PRIMARY KEY,
+    SubjectCode     NVARCHAR(10)   NOT NULL,
+    CourseNumber    NVARCHAR(10)   NOT NULL,
+    Title           NVARCHAR(200)  NOT NULL,
+    CourseDescription     NVARCHAR(MAX)  NULL,
+    Credits         DECIMAL(4,1)   NOT NULL CONSTRAINT DF_Course_Credits DEFAULT (3.0),
+    Capacity int not null default(0),
+    CONSTRAINT UK_Course_SubjectNumber UNIQUE (SubjectCode, CourseNumber),
+    CONSTRAINT CK_Course_Credits CHECK (Credits > 0 AND Credits <= 12.0)
+);
+GO
+
+create table Instructor (
+    InstructorID int identity(1,1) not null,
     FirstName nvarchar(50) not null,
     LastName nvarchar(50) not null,
-    Email nvarchar(255) not null,
-    PhoneNumber nvarchar(20) null,
-    PasswordHash nvarchar(255) not null,
-    UserRole nvarchar(20) not null,
-    CONSTRAINT PK_Appuser primary key (AppuserID),
-    CONSTRAINT UE_Appuser_Email UNIQUE (Email),
-    CONSTRAINT CK_Appuser_UserRole CHECK (UserRole in ('Student', 'Advisor', 'Admin'))
+    constraint pkInstructor primary key(InstructorID)
 );
 GO
 
-create table Student
-(
-    StudentID int identity(1,1),
-    AppuserID int not null,
-    TotalCreditsCompleted int not null default 0,
-    GraduateYear nvarchar(4) not null,
-    OverallGPA decimal(3,2) not null default 0.00,
-    AdjustedGPA decimal(3,2) not null default 0.00,
-    CONSTRAINT PK_Student primary key (StudentID),
-    CONSTRAINT FK_Student_Appuser foreign key (AppuserID) references Appuser(AppuserID)
+CREATE TABLE Section (
+    SectionID            INT IDENTITY(1,1) CONSTRAINT PK_Section PRIMARY KEY,
+    CourseID                    INT NOT NULL,
+    InstructorID                INT NOT NULL,
+    CRN                         NCHAR(5) NOT NULL,
+    SectionSemester      NVARCHAR(12) NOT NULL,
+    SectionYear          int NOT NULL,
+    SectionNumber               NVARCHAR(10) NULL,
+    RemainingOpenings           INT NOT NULL CONSTRAINT DF_Section_Seats DEFAULT (0),
+    SectionAverageRating DECIMAL(4,2) NOT NULL CONSTRAINT DF_Section_Avg DEFAULT (0.0),
+    CONSTRAINT FK_Section_Course FOREIGN KEY (CourseID)
+        REFERENCES Course(CourseID) ON DELETE CASCADE,
+    CONSTRAINT FK_Section_Instructor FOREIGN KEY (InstructorID)
+        REFERENCES Instructor(InstructorID) ON DELETE NO ACTION,
+    CONSTRAINT CK_Section_Sem CHECK (SectionSemester IN (N'Spring',N'Summer',N'Fall',N'Winter')),
+    CONSTRAINT CK_Section_Seats CHECK (RemainingOpenings >= 0),
+    CONSTRAINT CK_CourseOffering_Avg CHECK (SectionAverageRating >= 0 AND SectionAverageRating <= 5)
 );
-GO
-
--- Verify tables were created
-SELECT TABLE_NAME 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_TYPE = 'BASE TABLE'
-ORDER BY TABLE_NAME;
 GO
